80e14f95ea082cbc5ea02669aec005f2928e8e19
wolfgang <wolfgang@cwkuehl.de>
2022-01-27 20:51:55 +0100

    Load Icon from resource, no external file.

--- build.sh
+++ build.sh
@@ -20,8 +20,10 @@
 # sudo apt-get install -y dotnet-sdk-5.0
 # ==> OK
 dotnet publish ~/cs/csbp/CSBP/CSBP.csproj --configuration Release --framework net5.0 -p:Version=1.0.0.0
+#dotnet publish ~/cs/csbp/CSBP/CSBP.csproj --configuration Release --framework net5.0 --runtime ubuntu-x64 -p:Version=1.0.0.0
+#dotnet publish ~/cs/csbp/CSBP/CSBP.csproj --configuration Release --framework net5.0 --runtime win-x64 -p:Version=1.0.0.0
 
 cp -rf /home/wolfgang/cs/csbp/CSBP/bin/Release/net5.0/publish/ /opt/Haushalt/CSBP/
-cp -rf /home/wolfgang/cs/csbp/CSBP/Resources/ /opt/Haushalt/CSBP/publish
-rm /opt/Haushalt/CSBP/publish/Resources/T*
-rm /opt/Haushalt/CSBP/publish/Resources/M*
+#cp -rf /home/wolfgang/cs/csbp/CSBP/Resources/ /opt/Haushalt/CSBP/publish
+#rm /opt/Haushalt/CSBP/publish/Resources/T*
+#rm /opt/Haushalt/CSBP/publish/Resources/M*
