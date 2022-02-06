5dc2e64d91a30822a5cbb3888054be5af38e46b1
wolfgang <wolfgang@cwkuehl.de>
2022-02-06 20:44:10 +0100

    Fast build and complete deployment.

--- build.sh
+++ build.sh
@@ -20,9 +20,12 @@
 # sudo apt-get install -y dotnet-sdk-5.0
 # ==> OK
 dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net6.0 -r ubuntu-x64 --self-contained false -p:Version=1.1
-thirdlicense --project ./CSBP/CSBP.csproj
 #dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net6.0 -r ubuntu-x64 --self-contained true -p:Version=1.1
 #dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net6.0 -r win-x64 --self-contained true -p:Version=1.1
+#thirdlicense --project ./CSBP/CSBP.csproj
+#cd Asciidoc
+#./asciidoc-csbp.sh
+#cd ..
 
 cp -rf /home/wolfgang/cs/csbp/CSBP/bin/Release/net6.0/ubuntu-x64/publish/ /opt/Haushalt/CSBP/
 #cp -rf /home/wolfgang/cs/csbp/CSBP/Resources/ /opt/Haushalt/CSBP/publish
