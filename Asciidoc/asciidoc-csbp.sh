364e02c5a2cf701ecb6fa9c40b439cfe3e3d7349
wolfgang <wolfgang@cwkuehl.de>
2022-02-14 18:02:39 +0100

    Copy help file after generating.

--- Asciidoc/asciidoc-csbp.sh
+++ Asciidoc/asciidoc-csbp.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
 # sudo apt-get install -y asciidoctor
 # sudo gem update asciidoctor
 # sudo gem install asciidoctor-pdf
 asciidoctor -v -b html5 -o Csbp-Hilfe.html root/Csbp-Hilfe.adoc
 # asciidoctor-pdf -v --trace -b pdf -o Csbp-Hilfe.pdf root/Csbp-Hilfe.adoc
+cp -a Csbp-Hilfe.html /opt/Haushalt/CSBP
