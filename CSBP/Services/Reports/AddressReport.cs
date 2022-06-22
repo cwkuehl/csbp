a48d335e4ed849ec665493debd25d6bd548374d0
wolfgang <wolfgang@cwkuehl.de>
2022-06-22 22:19:50 +0200

    StyleCop changes.

--- CSBP/Services/Reports/AddressReport.cs
+++ CSBP/Services/Reports/AddressReport.cs
@@ -2,24 +2,21 @@
 // Copyright (c) cwkuehl.de. All rights reserved.
 // </copyright>
 
+namespace CSBP.Services.Reports;
+
 using System.Collections.Generic;
 using System.Text;
 using CSBP.Apis.Models;
 using CSBP.Base;
 using static CSBP.Base.Functions;
 
-namespace CSBP.Services.Reports
+/// <summary>Klasse für die Erstellung eines Adressen-Berichts.</summary>
+public partial class AddressReport : ReportBase
 {
-  /// <summary>Klasse für die Erstellung eines Adressen-Berichts.</summary>
-  public partial class AddressReport : ReportBase
+  /// <summary>Initializes a new instance of the <see cref="AddressReport"/> class.</summary>
+  public AddressReport()
   {
-    /// <summary>Holt oder setzt den Wert der Sitze mit Personen und Adressen.</summary>
-    public List<AdSitz> Sites { get; set; }
-
-    /// <summary>Konstruktor legt den Style fest.</summary>
-    public AddressReport()
-    {
-      Style = @"
+    Style = @"
 * {
  font-family: Arial;
  font-size: 10px;
@@ -48,104 +45,111 @@ namespace CSBP.Services.Reports
  padding 0;
 }
 ";
-    }
+  }
 
-    /// <summary>Internes Erzeugen des Reports.</summary>
-    protected override void DoGenerate()
+  /// <summary>Gets or sets the list of persons, sites and addresses.</summary>
+  public List<AdSitz> Sites { get; set; }
+
+  /// <summary>Internal generation of report.</summary>
+  protected override void DoGenerate()
+  {
+    if (Sites == null)
+      return;
+    xml.WriteStartElement("table");
+    xml.WriteAttributeString("class", "row");
+    xml.WriteStartElement("tr");
+    xml.WriteStartElement("td");
+    xml.WriteAttributeString("class", "column1");
+    var uid = "";
+    var anzahl = 0;
+    foreach (var s in Sites)
     {
-      if (Sites == null)
-        return;
-      xml.WriteStartElement("table");
-      xml.WriteAttributeString("class", "row");
-      xml.WriteStartElement("tr");
-      xml.WriteStartElement("td");
-      xml.WriteAttributeString("class", "column1");
-      var uid = "";
-      var anzahl = 0;
-      foreach (var s in Sites)
+      if (s?.Person.Uid != uid)
+        anzahl++;
+      if (anzahl % 2 == 1)
       {
         if (s?.Person.Uid != uid)
-          anzahl++;
-        if (anzahl % 2 == 1)
         {
-          if (s?.Person.Uid != uid)
-          {
-            xml.WriteStartElement("p");
-            xml.WriteAttributeString("class", "person");
-            xml.WriteString(GetPersonName(s.Person));
-            xml.WriteEndElement();
-          }
           xml.WriteStartElement("p");
-          xml.WriteAttributeString("class", "site");
-          xml.WriteString(GetSitzName(s));
+          xml.WriteAttributeString("class", "person");
+          xml.WriteString(GetPersonName(s.Person));
           xml.WriteEndElement();
         }
-        uid = s?.Person.Uid;
+        xml.WriteStartElement("p");
+        xml.WriteAttributeString("class", "site");
+        xml.WriteString(GetSitzName(s));
+        xml.WriteEndElement();
       }
-      xml.WriteEndElement();
-      xml.WriteStartElement("td");
-      xml.WriteAttributeString("class", "column2");
-      uid = "";
-      anzahl = 0;
-      foreach (var s in Sites)
+      uid = s?.Person.Uid;
+    }
+    xml.WriteEndElement();
+    xml.WriteStartElement("td");
+    xml.WriteAttributeString("class", "column2");
+    uid = "";
+    anzahl = 0;
+    foreach (var s in Sites)
+    {
+      if (s?.Person.Uid != uid)
+        anzahl++;
+      if (anzahl % 2 == 0)
       {
         if (s?.Person.Uid != uid)
-          anzahl++;
-        if (anzahl % 2 == 0)
         {
-          if (s?.Person.Uid != uid)
-          {
-            xml.WriteStartElement("p");
-            xml.WriteAttributeString("class", "person");
-            xml.WriteString(GetPersonName(s.Person));
-            xml.WriteEndElement();
-          }
           xml.WriteStartElement("p");
-          xml.WriteAttributeString("class", "site");
-          xml.WriteString(GetSitzName(s));
+          xml.WriteAttributeString("class", "person");
+          xml.WriteString(GetPersonName(s.Person));
           xml.WriteEndElement();
         }
-        uid = s?.Person.Uid;
+        xml.WriteStartElement("p");
+        xml.WriteAttributeString("class", "site");
+        xml.WriteString(GetSitzName(s));
+        xml.WriteEndElement();
       }
-      xml.WriteEndElement(); // td
-      xml.WriteEndElement(); // tr
-      xml.WriteEndElement(); // table
+      uid = s?.Person.Uid;
     }
+    xml.WriteEndElement(); // td
+    xml.WriteEndElement(); // tr
+    xml.WriteEndElement(); // table
+  }
 
-    private static string GetPersonName(AdPerson a)
-    {
-      if (a == null)
-        return "";
-      var sb = new StringBuilder();
-      sb.Append(" ", a.Praedikat).Append(" ", a.Name1).Append(" ", a.Name2).Append(", ", a.Vorname);
-      sb.Append(" (", a.Titel, ")").Append(", ", Functions.ToString(a.Geburt));
-      return sb.ToString();
-    }
+  /// <summary>
+  /// Gets a person string.
+  /// </summary>
+  /// <param name="a">Affected person.</param>
+  /// <returns>Person string.</returns>
+  private static string GetPersonName(AdPerson a)
+  {
+    if (a == null)
+      return "";
+    var sb = new StringBuilder();
+    sb.Append(" ", a.Praedikat).Append(" ", a.Name1).Append(" ", a.Name2).Append(", ", a.Vorname);
+    sb.Append(" (", a.Titel, ")").Append(", ", Functions.ToString(a.Geburt));
+    return sb.ToString();
+  }
 
-    private static string GetSitzName(AdSitz s)
+  private static string GetSitzName(AdSitz s)
+  {
+    var sb = new StringBuilder();
+    sb.Append(null, s.Name).Append(':');
+    if (s.Address != null)
     {
-      var sb = new StringBuilder();
-      sb.Append(null, s.Name).Append(':');
-      if (s.Address != null)
+      var a = s.Address;
+      if (a.Staat != "D")
       {
-        var a = s.Address;
-        if (a.Staat != "D")
-        {
-          sb.Append(" ", a.Staat, "-");
-        }
-        sb.Append(" ", a.Strasse);
-        sb.Append(" ", s.Postfach);
-        sb.Append(" ", a.HausNr);
-        sb.Append(" ", a.Plz);
-        sb.Append(" ", a.Ort);
+        sb.Append(" ", a.Staat, "-");
       }
-      sb.Append(" ", s.Telefon);
-      sb.Append(" ", s.Fax);
-      sb.Append(" ", s.Mobil);
-      sb.Append(" ", s.Email);
-      sb.Append(" ", s.Homepage);
-      sb.Append(" ", s.Bemerkung);
-      return sb.ToString();
+      sb.Append(" ", a.Strasse);
+      sb.Append(" ", s.Postfach);
+      sb.Append(" ", a.HausNr);
+      sb.Append(" ", a.Plz);
+      sb.Append(" ", a.Ort);
     }
+    sb.Append(" ", s.Telefon);
+    sb.Append(" ", s.Fax);
+    sb.Append(" ", s.Mobil);
+    sb.Append(" ", s.Email);
+    sb.Append(" ", s.Homepage);
+    sb.Append(" ", s.Bemerkung);
+    return sb.ToString();
   }
 }
--- CSBP/Services/Repositories/Base/RepositoryBase.cs
+++ CSBP/Services/Repositories/Base/RepositoryBase.cs
@@ -2,128 +2,127 @@
 // Copyright (c) cwkuehl.de. All rights reserved.
 // </copyright>
 
-namespace CSBP.Services.Repositories.Base
+namespace CSBP.Services.Repositories.Base;
+
+using System;
+using System.Data.Common;
+using CSBP.Apis.Services;
+using CSBP.Base;
+using Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Base class for repositories.
+/// </summary>
+public class RepositoryBase
 {
-  using System;
-  using System.Data.Common;
-  using CSBP.Apis.Services;
-  using CSBP.Base;
-  using Microsoft.EntityFrameworkCore;
+  /// <summary>
+  /// Initializes a new instance of the <see cref="RepositoryBase"/> class.
+  /// </summary>
+  public RepositoryBase()
+  {
+    Functions.MachNichts();
+  }
 
   /// <summary>
-  /// Basis-Klasse für Repositories.
+  /// Sets the columns Angelegt_Von and Angelegt_Am.
   /// </summary>
-  public class RepositoryBase
+  /// <param name="b">Entity as ModelBase.</param>
+  /// <param name="daten">Service data with time and user id.</param>
+  /// <param name="now">Affected time stamp.</param>
+  /// <param name="from">Affected user id.</param>
+  public static void MachAngelegt(ModelBase b, ServiceDaten daten, DateTime? now = null, string from = null)
   {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="RepositoryBase"/> class.
-    /// </summary>
-    public RepositoryBase()
-    {
-      Functions.MachNichts();
-    }
+    if (b == null || daten == null)
+      return;
+    if (from == null)
+      b.MachAngelegt(daten.Jetzt, daten.BenutzerId);
+    else
+      b.MachAngelegt(now, from);
+  }
 
-    /// <summary>
-    /// Liefert einen Datenbank-Context aus Service-Daten.
-    /// </summary>
-    /// <param name="daten">Service data for database access.</param>
-    /// <returns>Datenbank-Context aus Service-Daten.</returns>
-    protected static CsbpContext GetDb(ServiceDaten daten)
-    {
-      var db = daten.Context as CsbpContext;
-      return db;
-    }
+  /// <summary>
+  /// Sets the columns Geaendert_Von and Geaendert_Am,
+  /// only when the last entry in column Angelegt_Am or Geaendert_Am outdates AEND_ZEIT ticks.
+  /// </summary>
+  /// <param name="b">Entity as ModelBase.</param>
+  /// <param name="daten">Service data with time and user id.</param>
+  /// <param name="now">Affected time stamp.</param>
+  /// <param name="from">Affected user id.</param>
+  public static void MachGeaendert(ModelBase b, ServiceDaten daten, DateTime? now = null, string from = null)
+  {
+    if (b == null || daten == null)
+      return;
+    if (from == null)
+      b.MachGeaendert(daten.Jetzt, daten.BenutzerId);
+    else
+      b.MachGeaendert(now, from);
+  }
 
-    /// <summary>
-    /// Liefert eine Datenbank-Connection aus Service-Daten.
-    /// </summary>
-    /// <param name="daten">Service data for database access.</param>
-    /// <returns>Datenbank-Connection aus Service-Daten.</returns>
-    protected static DbConnection GetCon(ServiceDaten daten)
-    {
-      var db = daten.Context as DbContext;
-      var con = db.Database.GetDbConnection();
-      return con;
-    }
+  /// <summary>
+  /// Gets database context from service data.
+  /// </summary>
+  /// <param name="daten">Service data for database access.</param>
+  /// <returns>Database context from service data.</returns>
+  protected static CsbpContext GetDb(ServiceDaten daten)
+  {
+    var db = daten.Context as CsbpContext;
+    return db;
+  }
 
-    /// <summary>
-    /// Fügt einem Command einen Parameter hinzu.
-    /// </summary>
-    /// <param name="cmd">Betroffenes Command.</param>
-    /// <param name="name">Betroffener Parameter-Name.</param>
-    /// <param name="value">Betroffener Parameter-Wert.</param>
-    protected static void AddParam(DbCommand cmd, string name, object value)
-    {
-      var p = cmd.CreateParameter();
-      p.ParameterName = name;
-      p.Value = value;
-      cmd.Parameters.Add(p);
-    }
+  /// <summary>
+  /// Gets database connection string from service data.
+  /// </summary>
+  /// <param name="daten">Service data for database access.</param>
+  /// <returns>Database connection string from service data.</returns>
+  protected static DbConnection GetCon(ServiceDaten daten)
+  {
+    var db = daten.Context as DbContext;
+    var con = db.Database.GetDbConnection();
+    return con;
+  }
 
-    /// <summary>
-    /// Nachbildung der Funktion Like mit StartsWith, Contains und EndsWith.
-    /// </summary>
-    /// <param name="s">Zu prüfender String.</param>
-    /// <param name="exp">Betroffene Like-Expression.</param>
-    /// <returns></returns>
-    protected static bool Like(string s, string exp)
-    {
-      // The 'Like' method is not supported because the query has switched to client-evaluation.
-      // This usually happens when the arguments to the method cannot be translated to server.
-      // Rewrite the query to avoid client evaluation of arguments so that method can be translated to server.
-      if (!Functions.IsLike(exp))
-        return true;
-      if (string.IsNullOrEmpty(s))
-        return false;
-      var arr = exp.Split('%', StringSplitOptions.None);
-      if (!string.IsNullOrEmpty(arr[0]))
-        if (!s.StartsWith(arr[0], StringComparison.CurrentCultureIgnoreCase))
-          return false;
-      if (arr.Length > 1 && !string.IsNullOrEmpty(arr[^1]))
-        if (!s.EndsWith(arr[^1], StringComparison.CurrentCultureIgnoreCase))
-          return false;
-      for (var i = 1; i < arr.Length - 1; i++)
-      {
-        if (!string.IsNullOrEmpty(arr[i]) && !s.Contains(arr[i], StringComparison.CurrentCultureIgnoreCase))
-          return false;
-      }
+  /// <summary>
+  /// Adds parameter to command.
+  /// </summary>
+  /// <param name="cmd">Affected command.</param>
+  /// <param name="name">Affected parameter name.</param>
+  /// <param name="value">Affected parameter value.</param>
+  protected static void AddParam(DbCommand cmd, string name, object value)
+  {
+    var p = cmd.CreateParameter();
+    p.ParameterName = name;
+    p.Value = value;
+    cmd.Parameters.Add(p);
+  }
+
+  /// <summary>
+  /// Simulation of function like with StartsWith, Contains and EndsWith.
+  /// </summary>
+  /// <param name="s">Affected string.</param>
+  /// <param name="exp">Affected like expression.</param>
+  /// <returns>The string fulfills the like expression.</returns>
+  protected static bool Like(string s, string exp)
+  {
+    // The 'Like' method is not supported because the query has switched to client-evaluation.
+    // This usually happens when the arguments to the method cannot be translated to server.
+    // Rewrite the query to avoid client evaluation of arguments so that method can be translated to server.
+    if (!Functions.IsLike(exp))
       return true;
-      // return EF.Functions.Like(s, exp);
-    }
-
-    /// <summary>
-    /// Eintragungen in Spalten Angelegt_Von und Angelegt_Am.
-    /// </summary>
-    /// <param name="b">Entity als ModelBase.</param>
-    /// <param name="daten">Service-Daten mit Zeit und Benutzer-ID.</param>
-    /// <param name="now">Betroffener Zeitpunkt.</param>
-    /// <param name="from">Betroffene Benutzer-ID.</param>
-    public static void MachAngelegt(ModelBase b, ServiceDaten daten, DateTime? now = null, string from = null)
+    if (string.IsNullOrEmpty(s))
+      return false;
+    var arr = exp.Split('%', StringSplitOptions.None);
+    if (!string.IsNullOrEmpty(arr[0]))
+      if (!s.StartsWith(arr[0], StringComparison.CurrentCultureIgnoreCase))
+        return false;
+    if (arr.Length > 1 && !string.IsNullOrEmpty(arr[^1]))
+      if (!s.EndsWith(arr[^1], StringComparison.CurrentCultureIgnoreCase))
+        return false;
+    for (var i = 1; i < arr.Length - 1; i++)
     {
-      if (b == null || daten == null)
-        return;
-      if (from == null)
-        b.MachAngelegt(daten.Jetzt, daten.BenutzerId);
-      else
-        b.MachAngelegt(now, from);
+      if (!string.IsNullOrEmpty(arr[i]) && !s.Contains(arr[i], StringComparison.CurrentCultureIgnoreCase))
+        return false;
     }
-
-    /// <summary>
-    /// Eintragungen in Spalten Geaendert_Von und Geaendert_Am,
-    /// wenn der letzte Eintrag in den Spalten Angelegt_Am oder Geaendert_Am schon AEND_ZEIT Ticks her ist.
-    /// </summary>
-    /// <param name="b">Entity als ModelBase.</param>
-    /// <param name="daten">Service-Daten mit Zeit und Benutzer-ID.</param>
-    /// <param name="now">Betroffener Zeitpunkt.</param>
-    /// <param name="from">Betroffene Benutzer-ID.</param>
-    public static void MachGeaendert(ModelBase b, ServiceDaten daten, DateTime? now = null, string from = null)
-    {
-      if (b == null || daten == null)
-        return;
-      if (from == null)
-        b.MachGeaendert(daten.Jetzt, daten.BenutzerId);
-      else
-        b.MachGeaendert(now, from);
-    }
+    return true;
+    //// return EF.Functions.Like(s, exp);
   }
 }
