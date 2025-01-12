76f11a729b4f1ea40e84fff7d204299eb25f3160
wolfgang <wolfgang@cwkuehl.de>
2025-01-12 22:14:44 +0100

    TODO removed.

--- CSBP.Services/Base/Parameter.cs
+++ CSBP.Services/Base/Parameter.cs
@@ -56,7 +56,7 @@ public class Parameter
   /// </summary>
   static Parameter()
   {
-    // TODO Values dependant from client number.
+    // Values dependant from client number are directly read by function MaParameterRep.GetValue.
     Params = new Dictionary<string, Parameter>
     {
       // Storage in setting file
