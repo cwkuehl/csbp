3f89609ca4799c08cfde127896e45f6151e3f62e
wolfgang <wolfgang@cwkuehl.de>
2025-12-03 22:30:26 +0100

    Updated deploy script.

--- Install/#Deploy.sh
+++ Install/#Deploy.sh
@@ -20,39 +20,36 @@ build() {
   zip -rq "$2" csbp
 }
 
-thirdlicense --project ../CSBP/CSBP.csproj --output ../THIRD-PARTY-NOTICES.TXT
-cd ../Asciidoc
-./asciidoc-csbp.sh
+# Leider nur für .net8.0: dotnet tool install --global ThirdLicense --version 1.3.1
+#thirdlicense --project ../CSBP/CSBP.csproj --output ../THIRD-PARTY-NOTICES.TXT
+#cd ../Asciidoc
+#./asciidoc-csbp.sh
 cd ../Install
 # Browser cannot show html file in opt folder
 cp -a ../Asciidoc/Csbp-Hilfe.html /opt/Haushalt/CSBP
 # ab .net8 RID linux-x64
 
-dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net9.0 -r linux-x64 --self-contained true
-build "net9.0/linux-x64" "csbp-net6-ubuntu-x64-runtime.zip"
-#dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net8.0 -r linux-x64 --self-contained true
-#build "net8.0/linux-x64" "csbp-net6-ubuntu-x64-runtime.zip"
+dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net9.0 -r linux-arm64 --self-contained true
+build "net9.0/linux-arm64" "csbp-net9-linux-arm64-runtime.zip"
 dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net9.0 -r win-x64 --self-contained true
-build "net9.0/win-x64" "csbp-net6-win-x64-runtime.zip"
-#dotnet publish ~/cs/csbp/CSBP/CSBP.csproj -c Release -f net8.0 -r win-x64 --self-contained true
-#build "net8.0/win-x64" "csbp-net6-win-x64-runtime.zip"
+build "net9.0/win-x64" "csbp-net9-win-x64-runtime.zip"
 
-cp -a linux/#InstallUpdateCsbp.sh ./InstallUpdateCsbp.sh_.txt
-cp -a windows/#InstallUpdateCsbp.cmd ./InstallUpdateCsbp.cmd_.txt
-cp -a csbp/Csbp-Hilfe.html .
+#cp -a linux/#InstallUpdateCsbp.sh ./InstallUpdateCsbp.sh_.txt
+#cp -a windows/#InstallUpdateCsbp.cmd ./InstallUpdateCsbp.cmd_.txt
+#cp -a csbp/Csbp-Hilfe.html .
 
-cat << EOT >ftp.txt
-put InstallUpdateCsbp.sh_.txt
-put InstallUpdateCsbp.cmd_.txt
-put Csbp-Hilfe.html
-put csbp-net6-ubuntu-x64-runtime.zip
-put csbp-net6-win-x64-runtime.zip
-EOT
+#cat << EOT >ftp.txt
+#put InstallUpdateCsbp.sh_.txt
+#put InstallUpdateCsbp.cmd_.txt
+#put Csbp-Hilfe.html
+#put csbp-net9-linux-arm64-runtime.zip
+#put csbp-net9-win-x64-runtime.zip
+#EOT
 
 # Upload via sftp
-./#Upload.sh ftp.txt
+#./#Upload.sh ftp.txt
 
-rm -f ./ftp.txt
-rm -f ./InstallUpdateCsbp.sh_.txt
-rm -f ./InstallUpdateCsbp.cmd_.txt
-rm -f ./Csbp-Hilfe.html
+#rm -f ./ftp.txt
+#rm -f ./InstallUpdateCsbp.sh_.txt
+#rm -f ./InstallUpdateCsbp.cmd_.txt
+#rm -f ./Csbp-Hilfe.html
