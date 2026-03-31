9ef7df26b557f0c69df8c71736de64be99083c31
wolfgang <wolfgang@cwkuehl.de>
2026-03-31 16:54:55 +0200

    StatusTask class extended.

--- CSBP.Services/Base/StatusTask.cs
+++ CSBP.Services/Base/StatusTask.cs
@@ -41,12 +41,14 @@ public class StatusTask
   /// <param name="mandant">Affected tenant.</param>
   /// <param name="funktion">Affected function.</param>
   /// <param name="dateiname">Affected filename.</param>
-  public StatusTask(int mandant, string funktion, string? dateiname)
+  /// <param name="kurz">A value indicating whether the message should be short.</param>
+  public StatusTask(int mandant, string funktion, string? dateiname, bool kurz = false)
   {
     Mandant = mandant < 0 ? null : Functions.ToString(mandant);
     Mandant2 = null;
     Funktion = funktion;
     Dateiname = dateiname;
+    Kurz = kurz;
     StartTask();
   }
 
@@ -91,6 +93,38 @@ public class StatusTask
   public DateTime? Endzeit { get; private set; }
 
   /// <summary>
+  /// Gets a value indicating whether the message should be short.
+  /// </summary>
+  public bool Kurz { get; private set; }
+
+  /// <summary>
+  /// Liefert des Status aller gewünschten laufenden Funktionen auf allen Application Servern.
+  /// </summary>
+  /// <param name="mandant">Betroffener Mandant.</param>
+  /// <param name="funktionen">Betroffene Funktionen können null sein.</param>
+  /// <param name="kurz">True, wenn nur der aktuelle Taskstatus zurückgegeben werden soll.</param>
+  /// <returns>Status als String.</returns>
+  public static string GetStatus(int mandant, string[]? funktionen = null, bool kurz = false)
+  {
+      var suche = $"ST_{mandant}_*.txt";
+      var fliste = funktionen?.Select(a => $"_{a}_").ToArray();
+      var files = new DirectoryInfo(Pfad).GetFiles(suche).OrderByDescending(f => f.LastWriteTime).Select(f => f.FullName).ToList();
+      if (kurz)
+        files = files.Take(1).ToList();
+      var sb = new StringBuilder();
+      foreach (var file in files)
+      {
+          if (!file.Contains("ST_Abbruch_") && (fliste == null || Array.Exists(fliste, a => file.Contains(a))))
+          {
+              if (sb.Length > 0)
+                  sb.Append(Constants.CrLf).Append(Constants.CrLf);
+              sb.Append(File.ReadAllText(file));
+          }
+      }
+      return sb.ToString();
+  }
+
+  /// <summary>
   /// Liefert zurück, ob eine Funktion noch am Laufen ist.
   /// </summary>
   /// <param name="mandant">Betroffener Mandant kann null.</param>
@@ -106,6 +140,38 @@ public class StatusTask
           return true;
     }
     return false;
+  }
+
+  /// <summary>
+  /// Abbruch-Kennzeichen in allen lokalen laufenden Funktionen setzen.
+  /// Abbruch-Dateien für die anderen Application Server schreiben.
+  /// </summary>
+  /// <param name="mandant">Betroffener Mandant kann null.</param>
+  /// <param name="funktionen">Betroffene Funktionen können null sein.</param>
+  public static void Abbrechen(string? mandant = null, string[]? funktionen = null)
+  {
+    foreach (var f in Daten)
+    {
+      if ((mandant == null || f.Mandant == mandant) && (funktionen == null || Array.Exists(funktionen, a => a == f.Funktion)))
+        if (f.IsTAmLaufen())
+          f.SetAbbruch();
+    }
+    var m = string.IsNullOrEmpty(mandant) ? "M###" : mandant;
+    var fliste = funktionen == null ? new List<string>() : funktionen.Where(a => !string.IsNullOrEmpty(a)).ToList();
+    if (!fliste.Any())
+      fliste.Add("F###");
+    foreach (var f in fliste)
+    {
+      var dateiname = Path.Combine(Pfad, Functions.GetDateiname($"ST_Abbruch_{m}_{f}_{Servername}", true, true, true, "txt"));
+      try
+      {
+        File.WriteAllText(dateiname, Servername);
+      }
+      catch (Exception)
+      {
+        Functions.MachNichts();
+      }
+    }
   }
 
   /// <summary>
@@ -196,7 +262,7 @@ public class StatusTask
     var r = new ServiceErgebnis<StatusTask?>();
     var dateiname = temporaer ? null
       : Path.Combine(Pfad, Functions.GetDateiname($"ST_{mandant}_{funktion}_{Servername}", true, true, true, "txt"));
-    var f = new StatusTask(mandant, funktion, dateiname);
+    var f = new StatusTask(mandant, funktion, dateiname, true);
     r.Ergebnis = f;
     if (!temporaer)
     {
@@ -500,17 +566,25 @@ public class StatusTask
       if (daten.TryGetValue("Benutzer", out v) && v != null)
         sb.Append(tr).Append("User: ").Append(v);
     }
-    if (daten.TryGetValue("Meldung", out v) && v != null)
-      sb.Append(tr).Append(v);
-    if (daten.TryGetValue("Meldung1", out v) && v != null)
-      sb.Append(tr).Append(v);
-    if (daten.TryGetValue("Meldung2", out v) && v != null)
-      sb.Append(tr).Append(v);
-    if (daten.TryGetValue("Meldung3", out v) && v != null)
-      sb.Append(tr).Append(v);
-    if (daten.TryGetValue("Meldung4", out v) && v != null)
-      sb.Append(tr).Append(v);
-    if (!kurz)
+    if (!Endzeit.HasValue || !kurz)
+    {
+      if (daten.TryGetValue("Meldung", out v) && v != null)
+        sb.Append(tr).Append(v);
+      if (daten.TryGetValue("Meldung1", out v) && v != null)
+        sb.Append(tr).Append(v);
+      if (daten.TryGetValue("Meldung2", out v) && v != null)
+        sb.Append(tr).Append(v);
+      if (daten.TryGetValue("Meldung3", out v) && v != null)
+        sb.Append(tr).Append(v);
+      if (daten.TryGetValue("Meldung4", out v) && v != null)
+        sb.Append(tr).Append(v);
+    }
+    if (kurz)
+    {
+      if (Endzeit.HasValue)
+        sb.Append(tr).Append("Beendet");
+    }
+    else
     {
       if (daten.TryGetValue("Bust", out v) && v != null)
         sb.Append(tr).Append("Aktuelle Buchungsstelle: ").Append(v);
@@ -551,14 +625,14 @@ public class StatusTask
       return;
     try
     {
-      File.WriteAllText(Dateiname, GetStatus());
+      File.WriteAllText(Dateiname, GetStatus(Kurz));
     }
     catch (Exception)
     {
       // ServiceBase.Log.Error("WebServiceStatusFunktion write", ex);
       // Try again after waiting a while.
       Thread.Sleep(Functions.NextRandom(1000, 2000));
-      File.WriteAllText(Dateiname, GetStatus());
+      File.WriteAllText(Dateiname, GetStatus(Kurz));
     }
     Schreibzeit = j;
   }
